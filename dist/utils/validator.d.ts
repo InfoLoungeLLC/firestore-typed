17fab451cc87c81786340dab8874a83f8fccd8b4
HIDANO Masaki <masagon@info-lounge.jp>
2025-07-25 20:44:01 +0900

    Remove dist

--- dist/utils/validator.d.ts
+++
@@ -1,6 +0,0 @@
-import type { SerializedDocumentData } from '../types/firestore-typed.types';
-/**
- * Validates data using a provided validator function and wraps errors in FirestoreTypedValidationError
- */
-export declare function validateData<T extends SerializedDocumentData>(data: unknown, path: string, validator: (data: unknown) => T): T;
-//# sourceMappingURL=validator.d.ts.map
